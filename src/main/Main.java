4406cc5e84c319afa59626cd775192ef3c8989d2
Tendu <tendu2011@gmail.com>
2022-08-18 17:01:57 +0545

    KeyListener now working properly in mac.

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,11 +8,11 @@ public class Main {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("2D Adventure");
-        window.setLocationRelativeTo(null);
-        window.setVisible(true);
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
         window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
         gamePanel.startGameThread();
     }
 }
